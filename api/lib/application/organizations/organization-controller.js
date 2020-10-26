5288d8c8da1ec465a99eb4cc878e3db3e7641890
yahya-youness <you@octo.com>
2020-10-26 22:49:07 +0100

    [API] change buffer to payload with file path

--- api/lib/application/organizations/organization-controller.js
+++ api/lib/application/organizations/organization-controller.js
@@ -106,10 +106,9 @@ module.exports = {
 
   async importSchoolingRegistrationsFromSIECLE(request, h) {
     const organizationId = parseInt(request.params.id);
-    const buffer = request.payload;
     const { format } = request.query;
 
-    await usecases.importSchoolingRegistrationsFromSIECLEFormat({ organizationId, buffer, format });
+    await usecases.importSchoolingRegistrationsFromSIECLE({ organizationId, payload : request.payload, format });
     return h.response(null).code(204);
   },
 
