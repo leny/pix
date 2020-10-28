b6976b7d37de3d9f0dea102f6a16022681b353cf
Laura Bergoens <laura.bergoens@pix.fr>
2020-10-28 17:08:31 +0100

    feat: create read-model campaign analysis to compute per chunk

---
+++ api/lib/domain/read-models/CampaignAnalysis.js
@@ -0,0 +1,101 @@
+const _ = require('lodash');
+const recommendationService = require('../services/recommendation-service');
+
+class CampaignAnalysis {
+  constructor({
+    campaignId,
+    targetProfile,
+    tutorials,
+  } = {}) {
+    this.id = campaignId;
+    const maxSkillLevelInTargetProfile = targetProfile.maxSkillDifficulty;
+    this.campaignTubeRecommendations = targetProfile.tubes.map((tube) => {
+      const competence = targetProfile.getCompetence(tube.competenceId);
+      const area = targetProfile.getArea(competence.areaId);
+      const tutorialIds = _.uniq(_.flatMap(tube.skills, 'tutorialIds'));
+      const tubeTutorials = _.filter(tutorials, (tutorial) => tutorialIds.includes(tutorial.id));
+      return new CampaignTubeRecommendation({
+        campaignId: campaignId,
+        area,
+        competence,
+        tube,
+        maxSkillLevelInTargetProfile,
+        tutorials: tubeTutorials,
+      });
+    });
+  }
+
+  addValidatedKnowledgeElementsToTubeRecommendations(validatedKnowledgeElementsByTube) {
+    _.each(this.campaignTubeRecommendations, (campaignTubeRecommendation) => {
+      const tubeId = campaignTubeRecommendation.tubeId;
+      if (tubeId in validatedKnowledgeElementsByTube) {
+        campaignTubeRecommendation.addValidatedKnowledgeElements(validatedKnowledgeElementsByTube[tubeId]);
+      }
+    });
+  }
+
+  finalize(participantCount) {
+    _.each(this.campaignTubeRecommendations, (campaignTubeRecommendation) => {
+      campaignTubeRecommendation.finalize(participantCount);
+    });
+  }
+}
+
+class CampaignTubeRecommendation {
+  constructor({
+    campaignId,
+    area,
+    tube,
+    competence,
+    maxSkillLevelInTargetProfile,
+    tutorials,
+  } = {}) {
+    this.campaignId = campaignId;
+    this.tube = tube;
+    this.competenceId = competence.id;
+    this.competenceName = competence.name;
+    this.tubePracticalTitle = tube.practicalTitle;
+    this.areaColor = area.color;
+    this.maxSkillLevelInTargetProfile = maxSkillLevelInTargetProfile;
+    this.tutorials = tutorials;
+    this.cumulativeScore = 0;
+    this.participantCountSoFar = 0;
+    this.averageScore = null;
+  }
+
+  get tubeId() {
+    return this.tube.id;
+  }
+
+  get id() {
+    return `${this.campaignId}_${this.tubeId}`;
+  }
+
+  addValidatedKnowledgeElements(validatedKnowledgeElements) {
+    const knowledgeElementsByParticipant = _.toArray(_.groupBy(validatedKnowledgeElements, 'userId'));
+    this._computeCumulativeScore(knowledgeElementsByParticipant);
+    this.participantCountSoFar += knowledgeElementsByParticipant.length;
+  }
+
+  finalize(participantCount) {
+    if (participantCount) {
+      const participantCountWithoutKnowledgeElements = participantCount - this.participantCountSoFar;
+      const emptyKnowledgeElementsByParticipant = _.times(participantCountWithoutKnowledgeElements, () => []);
+      this._computeCumulativeScore(emptyKnowledgeElementsByParticipant);
+      this.averageScore = this.cumulativeScore / participantCount;
+    }
+  }
+
+  _computeCumulativeScore(knowledgeElementsByParticipant) {
+    this.cumulativeScore += _(knowledgeElementsByParticipant)
+      .values()
+      .sumBy((knowledgeElements) =>
+        recommendationService.computeRecommendationScore(
+          this.tube.skills,
+          this.maxSkillLevelInTargetProfile,
+          knowledgeElements,
+        ));
+  }
+}
+
+module.exports = CampaignAnalysis;
