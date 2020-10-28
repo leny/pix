9d09495d126256fb0509c11b4d3d95515bd96179
yahya-youness <you@octo.com>
2020-10-28 15:38:27 +0100

    [API] use buffer from file instead of hapi
    We create a buffer from the temporary file directly witch is better than using a hapi buffer

--- api/lib/domain/usecases/import-schooling-registrations-from-siecle.js
+++ api/lib/domain/usecases/import-schooling-registrations-from-siecle.js
@@ -1,27 +1,30 @@
 const { FileValidationError, SameNationalStudentIdInOrganizationError, SameNationalStudentIdInFileError } = require('../errors');
-const _ = require('lodash');
+const { isEmpty } = require('lodash');
+const fs = require('fs').promises;
 const SchoolingRegistrationParser = require('../../infrastructure/serializers/csv/schooling-registration-parser');
 
-module.exports = async function importSchoolingRegistrationsFromSIECLE({ organizationId, buffer, format, organizationRepository, schoolingRegistrationsXmlService, schoolingRegistrationRepository }) {
+const NO_SCHOOLING_REGISTRATIONS_FOUND = 'Aucune inscription d\'élève n\'a pu être importée depuis ce fichier.Vérifiez que le fichier est conforme.';
+const FILE_FORMAT_NOT_VALID = 'Format de fichier non valide.';
 
+module.exports = async function importSchoolingRegistrationsFromSIECLEFormat({ organizationId, payload, format, schoolingRegistrationsXmlService, schoolingRegistrationRepository, organizationRepository }) {
   let schoolingRegistrationData = [];
 
   if (format === 'xml') {
-    const { UAIFromSIECLE, resultFromExtraction } = schoolingRegistrationsXmlService.extractSchoolingRegistrationsInformationFromSIECLE(buffer);
     const organization = await organizationRepository.get(organizationId);
     if (UAIFromSIECLE !== organization.externalId) {
       throw new FileValidationError('Aucun étudiant n’a été importé. L’import n’est pas possible car l’UAI du fichier SIECLE ne correspond pas à celui de votre établissement. En cas de difficulté, contactez support.pix.fr.');
     }
     schoolingRegistrationData = resultFromExtraction;
   } else if (format === 'csv') {
+    const buffer = await fs.readFile(payload.path);
     const csvSiecleParser = new SchoolingRegistrationParser(buffer, organizationId);
     schoolingRegistrationData = csvSiecleParser.parse().registrations;
   } else {
-    throw new FileValidationError('Format de fichier non valide.');
+    throw new FileValidationError(FILE_FORMAT_NOT_VALID);
   }
 
-  if (_.isEmpty(schoolingRegistrationData)) {
-    throw new FileValidationError('Aucune inscription d’élève n’a pu être importée depuis ce fichier. Vérifiez que le fichier est conforme.');
+  if (isEmpty(schoolingRegistrationData)) {
+    throw new FileValidationError(NO_SCHOOLING_REGISTRATIONS_FOUND);
   }
 
   try {
